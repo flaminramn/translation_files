e6f21925d8a7aed409bed1479fc35a4598e8cc5a
flaminramn <flaminramn@gmail.com>
2026-02-28 00:12:51 -0500

    update

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     fetch(`https://wabapi.ddns.net/books/${id}`, {
        headers: {
-         "x-api-key": "DqHuL+l7"
+         "x-API-KEY": "DqHuL+l7"
        }
       })   
       .then(res => {
@@ -75,7 +75,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     fetch(`https://wabapi.ddns.net/books?page=${page}`{
       headers: {
-         "x-api-key": "DqHuL+l7"
+         "x-API-KEY": "DqHuL+l7"
       }
       })
       .then(res => {
